a06f0aea0c2ed5bb8f8ddf435d1f002851c33dc8
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-01-06 14:54:10 +0100

    Added missing return before class

--- meilisearch/client.py
+++ meilisearch/client.py
@@ -6,6 +6,7 @@ from meilisearch.sys_info import SysInfo
 from meilisearch.stat import Stat
 from meilisearch.version import Version
 
+
 class Client(Health, Key, SysInfo, Version):
     """
     A client for the meilisearch API
