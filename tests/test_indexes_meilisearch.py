51964e79b4bd603e3831fc398f41add15bd3c1c0
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-01-06 14:53:16 +0100

    Fix broken test import

--- tests/test_indexes_meilisearch.py
+++ tests/test_indexes_meilisearch.py
@@ -4,11 +4,11 @@ import time
 import json
 import inspect
 
-import meilisearch
-
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
+
+import meilisearch
 
 
 class TestIndexes:
