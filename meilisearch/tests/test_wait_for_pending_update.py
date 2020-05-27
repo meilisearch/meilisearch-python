8784be9e977e61d710cfe74722aa5740a4af611e
Samuel Jimenez <sjimenezre@gmail.com>
2020-05-27 13:41:59 +0200

    Add test with interval set to 0

--- meilisearch/tests/test_wait_for_pending_update.py
+++ meilisearch/tests/test_wait_for_pending_update.py
@@ -7,9 +7,12 @@ from meilisearch.tests import BASE_URL, MASTER_KEY
 class TestUpdate:
     client = meilisearch.Client(BASE_URL, MASTER_KEY)
     index = None
+    dataset_file = open("datasets/small_movies.json", "r")
+    dataset_json = None
 
     def setup_class(self):
         self.index = self.client.create_index(uid='indexUID')
+        self.dataset_json = json.loads(self.dataset_file.read())
 
     def teardown_class(self):
         self.index.delete()
@@ -28,11 +31,9 @@ class TestUpdate:
         with pytest.raises(TimeoutError):
             self.index.wait_for_pending_update(2, timeout_in_ms=0)
 
-    def test_wait_for_pending_update_interval(self):
+    def test_wait_for_pending_update_interval_custom(self):
         """Tests call to wait for an update with custom interval"""
-        dataset_file = open("datasets/small_movies.json", "r")
-        dataset_json = json.loads(dataset_file.read())
-        response = self.index.add_documents(dataset_json)
+        response = self.index.add_documents(self.dataset_json)
         assert 'updateId' in response
         start_time = datetime.now()
         wait_update = self.index.wait_for_pending_update(
@@ -45,3 +46,17 @@ class TestUpdate:
         assert 'status' in wait_update
         assert wait_update['status'] != 'enqueued'
         assert time_delta.seconds >= 1
+
+    def test_wait_for_pending_update_interval_zero(self):
+        """Tests call to wait for an update with custom interval"""
+        response = self.index.add_documents(self.dataset_json)
+        assert 'updateId' in response
+        start_time = datetime.now()
+        wait_update = self.index.wait_for_pending_update(
+            response['updateId'],
+            interval_in_ms=0,
+            timeout_in_ms=6000
+        )
+        assert isinstance(wait_update, object)
+        assert 'status' in wait_update
+        assert wait_update['status'] != 'enqueued'
