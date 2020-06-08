e73740dd2b9fae5f131066659deeb0faea12ecd0
Clémentine Urquizar <clementine@meilisearch.com>
2020-06-08 14:20:14 +0200

    Update meilisearch/tests/__init__.py
    Co-authored-by: Samuel Jimenez <sjimenezre@gmail.com>

--- meilisearch/tests/__init__.py
+++ meilisearch/tests/__init__.py
@@ -3,6 +3,5 @@ BASE_URL = 'http://127.0.0.1:7700'
 
 def clear_all_indexes(client):
     indexes = client.get_indexes()
-    uids = [index['uid'] for index in indexes]
-    for uid in uids:
-        client.get_index(uid).delete()
+    for index in indexes:
+        client.get_index(index['uid']).delete()
