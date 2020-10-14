b45991aed79a750b16e9adb0edd3ee615b6fcabc
Samuel Jimenez <sjimenezre@gmail.com>
2020-10-14 11:02:26 +0200

    Add wait_for_dump_creation for crate_dump test

--- meilisearch/tests/client/test_client_dumps.py
+++ meilisearch/tests/client/test_client_dumps.py
@@ -19,24 +19,25 @@ class TestClientDumps:
         self.dataset_file = open('./datasets/small_movies.json', 'r')
         self.dataset_json = json.loads(self.dataset_file.read())
         self.dataset_file.close()
+        response = self.index.add_documents(self.dataset_json, primary_key='id')
+        self.index.wait_for_pending_update(response['updateId'])
 
     def test_dump_creation(self):
         """Tests the creation of a MeiliSearch dump"""
         dump = self.client.create_dump()
         assert dump['uid'] is not None
         assert dump['status'] == 'processing'
+        wait_for_dump_creation(self.client, dump['uid'])
 
     def test_dump_status_route(self):
         """Tests the route for getting a MeiliSearch dump status"""
-        response = self.index.add_documents(self.dataset_json, primary_key='id')
-        self.index.wait_for_pending_update(response['updateId'])
         dump = self.client.create_dump()
         assert dump['uid'] is not None
         assert dump['status'] == 'processing'
         dump_status = self.client.get_dump_status(dump['uid'])
         assert dump_status['uid'] is not None
         assert dump_status['status'] == 'processing'
-        wait_for_dump_creation(self.client, dump_status['uid'])
+        wait_for_dump_creation(self.client, dump['uid'])
 
     def test_dump_status_nonexistent_uid_raises_error(self):
         """Tests the route for getting a nonexistent dump status"""
