c298ffe53eabe6a4461f14373fecd9c183257331
Clémentine Urquizar <clementine@meilisearch.com>
2020-10-14 15:16:49 +0200

    Update the version in setup.py for the next release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         "requests"
     ],
     name="meilisearch",
-    version="0.12.3",
+    version="0.12.4",
     author="Charlotte Vermandel",
     author_email="charlotte@meilisearch.com",
     description="The python client for MeiliSearch API.",
