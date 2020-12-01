b0d97b9736d462e1240e7a54965eea37b53307f6
kamyar <932743+kamyar@users.noreply.github.com>
2020-12-01 23:53:50 +0100

    Improve setting test from suggestions
    Co-authored-by: Clémentine Urquizar <clementine@meilisearch.com>

--- meilisearch/tests/settings/test_settings.py
+++ meilisearch/tests/settings/test_settings.py
@@ -46,6 +46,21 @@ def test_update_settings(empty_index):
 def test_reset_settings(empty_index):
     """Tests resetting default settings"""
     index = empty_index()
+    # Update settings first
+    response = index.update_settings(NEW_SETTINGS)
+    update = index.wait_for_pending_update(response['updateId'])
+    assert update['status'] == 'processed'
+    # Check the settings have been correctly updated
+    response = index.get_settings()
+    for rule in NEW_SETTINGS['rankingRules']:
+        assert rule in response['rankingRules']
+    assert response['distinctAttribute'] is None
+    for attribute in NEW_SETTINGS['searchableAttributes']:
+        assert attribute in response['searchableAttributes']
+    assert response['displayedAttributes'] == ['*']
+    assert response['stopWords'] == []
+    assert response['synonyms'] == {}
+    # Check the reset of the settings
     response = index.reset_settings()
     assert isinstance(response, object)
     assert 'updateId' in response
