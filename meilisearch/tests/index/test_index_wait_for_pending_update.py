a02f26ebfb51f045cda721f91e87ba25a265afc6
Samuel Jimenez <sjimenezre@gmail.com>
2020-12-29 21:36:00 +0100

    Fix MeiliSearchTimoutError typo

--- meilisearch/tests/index/test_index_wait_for_pending_update.py
+++ meilisearch/tests/index/test_index_wait_for_pending_update.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 import pytest
-from meilisearch.errors import MeiliSearchTimeOutError
+from meilisearch.errors import MeiliSearchTimeoutError
 
 def test_wait_for_pending_update_default(index_with_documents):
     """Tests waiting for an update with default parameters."""
@@ -16,7 +16,7 @@ def test_wait_for_pending_update_default(index_with_documents):
 
 def test_wait_for_pending_update_timeout(index_with_documents):
     """Tests timeout risen by waiting for an update."""
-    with pytest.raises(MeiliSearchTimeOutError):
+    with pytest.raises(MeiliSearchTimeoutError):
         index_with_documents().wait_for_pending_update(2, timeout_in_ms=0)
 
 def test_wait_for_pending_update_interval_custom(index_with_documents, small_movies):
