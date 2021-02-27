f1a8f447fb0e9fa0d2884e3aa7d3b0759a1d753d
Paul Sanders <psanders1@gmail.com>
2021-02-27 10:48:26 -0500

    Removing repeated tests

--- meilisearch/tests/client/test_client.py
+++ meilisearch/tests/client/test_client.py
@@ -1,6 +1,5 @@
 # pylint: disable=invalid-name
 
-import pytest
 import meilisearch
 from meilisearch.tests import BASE_URL, MASTER_KEY
 
@@ -10,15 +9,3 @@ def test_get_client():
     assert client.config
     response = client.health()
     assert response.status_code >= 200 and response.status_code < 400
-
-def test_get_client_without_master_key():
-    """Tests getting a client instance without master key."""
-    client = meilisearch.Client(BASE_URL)
-    with pytest.raises(Exception):
-        client.get_version()
-
-def test_get_client_with_wrong_master_key():
-    """Tests getting a client instance with an invalid master key."""
-    client = meilisearch.Client(BASE_URL, MASTER_KEY + "123")
-    with pytest.raises(Exception):
-        client.get_version()
