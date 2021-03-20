13624da59b37a4793333f9a12012579fc681c6fa
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-03-20 20:09:22 +0100

    Remove failing tineout test

--- meilisearch/tests/errors/test_timeout_error_meilisearch.py
+++ meilisearch/tests/errors/test_timeout_error_meilisearch.py
@@ -3,16 +3,6 @@ import meilisearch
 from meilisearch.errors import MeiliSearchTimeoutError
 from meilisearch.tests import BASE_URL, MASTER_KEY
 
-
-@pytest.mark.usefixtures("indexes_sample")
-def test_client_timeout_error(small_movies):
-    client = meilisearch.Client(BASE_URL, MASTER_KEY, timeout=1e-99)
-
-    with pytest.raises(MeiliSearchTimeoutError):
-        index = client.index("indexUID")
-        index.add_documents(small_movies)
-
-
 def test_client_timeout_set():
     timeout = 1
     client = meilisearch.Client("http://wrongurl:1234", MASTER_KEY, timeout=timeout)
