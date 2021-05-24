28fa29b28ff967226786b6c87e6fb484d0e876fd
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2021-05-24 21:07:40 +0200

    Update to v0.14.3
    wait for #261

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         "requests"
     ],
     name="meilisearch",
-    version="0.14.2",
+    version="0.14.3",
     author="Charlotte Vermandel",
     author_email="charlotte@meilisearch.com",
     description="The python client for MeiliSearch API.",
