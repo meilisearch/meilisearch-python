f254acacd04cf40213e893d9561048f11fb2ccab
alallema <amelie@meilisearch.com>
2022-02-22 11:57:20 +0100

    Adding tests

--- meilisearch/client.py
+++ meilisearch/client.py
@@ -472,7 +472,7 @@ class Client():
     def generate_tenant_token(
         self,
         search_rules: Dict[str, Any],
-        expired_at: Optional[float] = None,
+        expires_at: Optional[int] = None,
         api_key: Optional[str] = None
     ) -> str:
         """Generate a JWT token for the use of multitenancy.
@@ -483,9 +483,9 @@ class Client():
             A Dictionary or an object which contains the rules to be enforced at search time for all or specific
             accessible indexes for the signing API Key.
             In the specific case of you want to have any restrictions you can also use a array ["*"].
-        expired_at (optional):
+        expires_at (optional):
             Date and time when the key will expire.
-            Note that if an expired_at value is included it should a `timestamp`.
+            Note that if an expires_at value is included it should a `timestamp`.
         api_key (optional):
             The API key parent of the token. If you let it empty the client API Key will be used.
 
@@ -493,11 +493,8 @@ class Client():
         -------
         jwt_token:
            A string containing the jwt tenant token.
-
-        Raises
-        ------
-        MeiliSearchApiError
-            An error containing details about why Meilisearch can't process your request. Meilisearch error codes are described here: https://docs.meilisearch.com/errors/#meilisearch-errors
+           Note: If your token does not work remember that the searchrules is madatory and should be well formatted.
+           `exp` must be a timestamp in the future.
         """
         # Standard JWT header for encryption with SHA256/HS256 algorithm
         header = {
@@ -511,7 +508,7 @@ class Client():
         payload = {
             'apiKeyPrefix': api_key[0:8],
             'searchRules': search_rules,
-            'exp': expired_at if expired_at is not None else None
+            'exp': expires_at if expires_at is not None else None
         }
 
         # Serialize the header and the payload
--- tests/client/test_client_tenant_token.py
+++ tests/client/test_client_tenant_token.py
@@ -1,8 +1,10 @@
 # pylint: disable=invalid-name
 
 from re import search
+import pytest
 import meilisearch
 from tests import BASE_URL, MASTER_KEY
+from meilisearch.errors import MeiliSearchApiError
 import datetime
 
 def test_generate_tenant_token_with_search_rules(get_private_key, index_with_documents):
@@ -33,17 +35,17 @@ def test_generate_tenant_token_with_api_key(client, get_private_key, index_with_
     assert len(response['hits']) == 20
     assert response['query'] == ''
 
-def test_generate_tenant_token_with_expired_at(client, get_private_key, index_with_documents):
+def test_generate_tenant_token_with_expires_at(client, get_private_key, index_with_documents):
     """Tests create a tenant token with only search rules."""
     index_with_documents()
     key = get_private_key
 
     client = meilisearch.Client(BASE_URL, key['key'])
 
-    tomorrow = datetime.datetime.now() + datetime.timedelta(days=1, hours=3)
+    tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
     timestamp = datetime.datetime.timestamp(tomorrow)
 
-    token = client.generate_tenant_token(search_rules=["*"], expired_at=int(timestamp))
+    token = client.generate_tenant_token(search_rules=["*"], expires_at=int(timestamp))
 
     token_client = meilisearch.Client(BASE_URL, token)
     response = token_client.index('indexUID').search('')
@@ -61,7 +63,32 @@ def test_generate_tenant_token_without_search_rules(get_private_key, index_with_
     token = client.generate_tenant_token(search_rules='')
 
     token_client = meilisearch.Client(BASE_URL, token)
-    response = token_client.index('indexUID').search('')
-    assert isinstance(response, dict)
-    assert len(response['hits']) == 20
-    assert response['query'] == ''
+    with pytest.raises(MeiliSearchApiError):
+        token_client.index('indexUID').search('')
+
+def test_generate_tenant_token_with_master_key(client, get_private_key, index_with_documents):
+    """Tests create a tenant token with only search rules."""
+    index_with_documents()
+    key = get_private_key
+
+    token = client.generate_tenant_token(search_rules=['*'])
+
+    token_client = meilisearch.Client(BASE_URL, token)
+    with pytest.raises(MeiliSearchApiError):
+        token_client.index('indexUID').search('')
+
+def test_generate_tenant_token_with_bad_expires_at(client, get_private_key, index_with_documents):
+    """Tests create a tenant token with only search rules."""
+    index_with_documents()
+    key = get_private_key
+
+    client = meilisearch.Client(BASE_URL, key['key'])
+
+    yesterday = datetime.datetime.now() + datetime.timedelta(days=-1)
+    timestamp = datetime.datetime.timestamp(yesterday)
+
+    token = client.generate_tenant_token(search_rules=["*"], expires_at=int(timestamp))
+
+    token_client = meilisearch.Client(BASE_URL, token)
+    with pytest.raises(MeiliSearchApiError):
+        token_client.index('indexUID').search('')
