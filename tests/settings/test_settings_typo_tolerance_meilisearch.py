f0eca613608d93ba2ee3397644a763fb4f083eb4
alallema <amelie@meilisearch.com>
2022-04-20 10:28:12 +0200

    Fix tests

--- tests/settings/test_settings_typo_tolerance_meilisearch.py
+++ tests/settings/test_settings_typo_tolerance_meilisearch.py
@@ -1,5 +1,5 @@
-TYPO_TOLERANCE =   {
-    'enabled': 'true',
+DEFAUT_TYPO_TOLERANCE =   {
+    'enabled': True,
     'minWordLengthForTypo': {
         'oneTypo': 5,
         'twoTypos': 9,
@@ -8,36 +8,46 @@ TYPO_TOLERANCE =   {
     'disableOnAttributes': [],
 }
 
+NEW_TYPO_TOLERANCE =   {
+    'enabled': True,
+    'minWordLengthForTypo': {
+        'oneTypo': 6,
+        'twoTypos': 10,
+    },
+    'disableOnWords': [],
+    'disableOnAttributes': ['title'],
+}
+
 def test_get_typo_tolerance_default(empty_index):
     """Tests getting default typo_tolerance."""
     response = empty_index().get_typo_tolerance()
     assert isinstance(response, dict)
-    assert response == {}
+    assert response == DEFAUT_TYPO_TOLERANCE
 
 def test_update_typo_tolerance(empty_index):
     """Tests updating typo_tolerance."""
     index = empty_index()
-    response = index.update_typo_tolerance(TYPO_TOLERANCE)
+    response = index.update_typo_tolerance(NEW_TYPO_TOLERANCE)
     assert isinstance(response, dict)
     assert 'uid' in response
     update = index.wait_for_task(response['uid'])
     assert update['status'] == 'succeeded'
     response = index.get_typo_tolerance()
     assert isinstance(response, dict)
-    for typo_tolerance in TYPO_TOLERANCE:
+    for typo_tolerance in NEW_TYPO_TOLERANCE:
         assert typo_tolerance in response
 
 def test_reset_typo_tolerance(empty_index):
     """Tests resetting the typo_tolerance setting to its default value."""
     index = empty_index()
     # Update the settings first
-    response = index.update_typo_tolerance(TYPO_TOLERANCE)
+    response = index.update_typo_tolerance(NEW_TYPO_TOLERANCE)
     update = index.wait_for_task(response['uid'])
     assert update['status'] == 'succeeded'
     # Check the settings have been correctly updated
     response = index.get_typo_tolerance()
     assert isinstance(response, dict)
-    for typo_tolerance in TYPO_TOLERANCE:
+    for typo_tolerance in NEW_TYPO_TOLERANCE:
         assert typo_tolerance in response
     # Check the reset of the settings
     response = index.reset_typo_tolerance()
@@ -46,4 +56,4 @@ def test_reset_typo_tolerance(empty_index):
     update = index.wait_for_task(response['uid'])
     assert update['status'] == 'succeeded'
     response = index.get_typo_tolerance()
-    assert response == {}
+    assert response == DEFAUT_TYPO_TOLERANCE
