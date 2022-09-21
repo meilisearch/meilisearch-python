cd7bf62ceb7c63862a37569d938cbbe3ad657da7
Amélie <alallema@users.noreply.github.com>
2022-09-21 23:59:14 +0200

    Update setup.py
    Co-authored-by: Paul Sanders <psanders1@gmail.com>

--- setup.py
+++ setup.py
@@ -21,12 +21,13 @@ setup(
     project_urls={"Documentation": "https://docs.meilisearch.com/",},
     keywords="search python meilisearch",
     platform="any",
-    classifiers=[
+classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
