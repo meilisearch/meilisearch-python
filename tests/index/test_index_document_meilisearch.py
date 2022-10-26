f76dd93c86b3fda1cec378d2e2e36e1c57c84801
adripo <26493496+adripo@users.noreply.github.com>
2022-10-26 21:04:20 +0200

    test: create test add_documents empty string ref: #585

--- tests/index/test_index_document_meilisearch.py
+++ tests/index/test_index_document_meilisearch.py
@@ -25,6 +25,15 @@ def test_add_documents(empty_index, small_movies):
     assert index.get_primary_key() == "id"
     assert update.status == "succeeded"
 
+def test_add_documents_empty(empty_index):
+    """Tests adding empty string as documents to a clean index."""
+    index = empty_index()
+    with pytest.raises(Exception) as e_info:
+        response = index.add_documents('')
+    assert e_info.value.message == "A json payload is missing."
+    assert e_info.value.code == "missing_payload"
+    assert e_info.value.type == "invalid_request"
+    assert e_info.value.link == "https://docs.meilisearch.com/errors#missing_payload"
 
 @pytest.mark.parametrize("batch_size", [2, 3, 1000])
 @pytest.mark.parametrize(
