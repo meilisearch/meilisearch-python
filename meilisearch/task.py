71d76994110cb65415761aa18501e2ef82e8f0d6
Azanul <azanulhaque@gmail.com>
2023-02-01 10:37:14 +0000

    lint

--- meilisearch/task.py
+++ meilisearch/task.py
@@ -14,7 +14,7 @@ from meilisearch.models.task import TaskInfo
 class TaskHandler:
     """
     A class covering the Meilisearch Task API
-    
+
     The task class gives access to all task routes and gives information about the progress of asynchronous operations.
     https://docs.meilisearch.com/reference/api/tasks.html
     """
