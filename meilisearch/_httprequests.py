f364935b12256eb8593399466ed6d82f529e4d50
Shiva Gaire <geeksambhu@gmail.com>
2023-12-06 23:14:01 +0545

    Incorporate PR feedback

--- meilisearch/_httprequests.py
+++ meilisearch/_httprequests.py
@@ -27,7 +27,7 @@ class HttpRequests:
         self,
         http_method: Callable,
         path: str,
-        body: Optional[Union[Dict[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
+        body: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
         content_type: Optional[str] = None,
     ) -> Any:
         if content_type:
@@ -67,7 +67,7 @@ class HttpRequests:
     def post(
         self,
         path: str,
-        body: Optional[Union[Dict[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
+        body: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
         content_type: Optional[str] = "application/json",
     ) -> Any:
         return self.send_request(requests.post, path, body, content_type)
@@ -75,7 +75,7 @@ class HttpRequests:
     def patch(
         self,
         path: str,
-        body: Optional[Union[Dict[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
+        body: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
         content_type: Optional[str] = "application/json",
     ) -> Any:
         return self.send_request(requests.patch, path, body, content_type)
@@ -83,7 +83,7 @@ class HttpRequests:
     def put(
         self,
         path: str,
-        body: Optional[Union[Dict[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
+        body: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]], List[str], str]] = None,
         content_type: Optional[str] = "application/json",
     ) -> Any:
         return self.send_request(requests.put, path, body, content_type)
@@ -91,7 +91,7 @@ class HttpRequests:
     def delete(
         self,
         path: str,
-        body: Optional[Union[Dict[str, Any], Sequence[Mapping[str, Any]], List[str]]] = None,
+        body: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]], List[str]]] = None,
     ) -> Any:
         return self.send_request(requests.delete, path, body)
 
