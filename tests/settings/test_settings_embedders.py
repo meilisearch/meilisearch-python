b08caee6a668e221619e663d170330bb3058fc63
CaroFG <48251481+CaroFG@users.noreply.github.com>
2024-02-07 11:02:10 +0100

    Use new_embedders fixture
    Co-authored-by: Paul Sanders <psanders1@gmail.com>

--- tests/settings/test_settings_embedders.py
+++ tests/settings/test_settings_embedders.py
@@ -21,24 +21,23 @@ def test_get_default_embedders(empty_index):
 
 
 @pytest.mark.usefixtures("enable_vector_search")
-def test_update_embedders_with_user_provided_source(empty_index):
+def test_update_embedders_with_user_provided_source(new_embedders, empty_index):
     """Tests updating embedders."""
     index = empty_index()
-    response_update = index.update_embedders(NEW_EMBEDDERS)
+    response_update = index.update_embedders(new_embedders)
     update = index.wait_for_task(response_update.task_uid)
     response_get = index.get_embedders()
-    print(response_get)
     assert update.status == "succeeded"
-    assert response_get == Embedders(embedders=NEW_EMBEDDERS)
+    assert response_get == Embedders(embedders=new_embedders)
 
 
 @pytest.mark.usefixtures("enable_vector_search")
-def test_reset_embedders(empty_index):
+def test_reset_embedders(new_embedders, empty_index):
     """Tests resetting the typo_tolerance setting to its default value."""
     index = empty_index()
 
     # Update the settings
-    response_update = index.update_embedders(NEW_EMBEDDERS)
+    response_update = index.update_embedders(new_embedders)
     update1 = index.wait_for_task(response_update.task_uid)
     # Get the setting after update
     response_get = index.get_embedders()
@@ -49,6 +48,6 @@ def test_reset_embedders(empty_index):
     response_last = index.get_embedders()
 
     assert update1.status == "succeeded"
-    assert response_get == Embedders(embedders=NEW_EMBEDDERS)
+    assert response_get == Embedders(embedders=new_embedders)
     assert update2.status == "succeeded"
     assert response_last is None
