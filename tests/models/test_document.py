2030e1bdd658d54090d9037581fa5d5856b1612b
nnethercott <nathaniel@deepomatic.com>
2025-06-09 15:38:50 +0200

    Update Document model and fix tests

--- tests/models/test_document.py
+++ tests/models/test_document.py
@@ -6,9 +6,15 @@ import pytest
 from meilisearch.models.document import Document
 
 
+def test_doc_init():
+    d = {"field1": "test 1", "fiels2": "test 2"}
+    document = Document(d)
+    assert dict(document) == d
+
+
 def test_getattr():
     document = Document({"field1": "test 1", "fiels2": "test 2"})
-    assert document.__getattr__("field1") == "field1"
+    assert document.__getattr__("field1") == "test 1"
 
 
 def test_getattr_not_found():
@@ -18,11 +24,5 @@ def test_getattr_not_found():
 
 
 def test_iter():
-    # I wrote a test what what this does, but I have a feeling this isn't actually what it was
-    # expected to do when written as it doesn't really act like I would expect an iterator to act.
     document = Document({"field1": "test 1", "fiels2": "test 2"})
-
-    assert next(document.__iter__()) == (
-        "_Document__doc",
-        {"field1": "test 1", "fiels2": "test 2"},
-    )
+    assert list(iter(document)) == [("field1", "test 1"), ("fiels2", "test 2")]
