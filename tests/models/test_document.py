4cb403c6bfd43771cc1944e39968b66ce5536182
nnethercott <nathaniel@deepomatic.com>
2025-06-09 16:22:03 +0200

    apply coderabbit suggestions

--- tests/models/test_document.py
+++ tests/models/test_document.py
@@ -7,22 +7,22 @@ from meilisearch.models.document import Document
 
 
 def test_doc_init():
-    d = {"field1": "test 1", "fiels2": "test 2"}
+    d = {"field1": "test 1", "field2": "test 2"}
     document = Document(d)
     assert dict(document) == d
 
 
 def test_getattr():
-    document = Document({"field1": "test 1", "fiels2": "test 2"})
+    document = Document({"field1": "test 1", "field2": "test 2"})
     assert document.__getattr__("field1") == "test 1"
 
 
 def test_getattr_not_found():
-    document = Document({"field1": "test 1", "fiels2": "test 2"})
+    document = Document({"field1": "test 1", "field2": "test 2"})
     with pytest.raises(AttributeError):
         document.__getattr__("bad")
 
 
 def test_iter():
-    document = Document({"field1": "test 1", "fiels2": "test 2"})
-    assert list(iter(document)) == [("field1", "test 1"), ("fiels2", "test 2")]
+    document = Document({"field1": "test 1", "field2": "test 2"})
+    assert list(iter(document)) == [("field1", "test 1"), ("field2", "test 2")]
